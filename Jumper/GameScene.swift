ae15fad35d7f765634164d1fb2faac8156ce106e
Nick Maslov <kategorychnost@gmail.com>
2021-10-06 14:27:22 -0400

    Added font, created game label, created infinite backgrounds, object animations and physical body

--- Jumper/GameScene.swift
+++ Jumper/GameScene.swift
@@ -10,12 +10,27 @@ import GameplayKit
 
 class GameScene: SKScene {
     
-    private var label : SKLabelNode?
-    private var spinnyNode : SKShapeNode?
+    var sky: EndlessBackground!
+    var forest: EndlessBackground!
+    var ground: EndlessBackground!
+    var girl: SKSpriteNode!
     
     override func didMove(to view: SKView) {
+        sky = EndlessBackground(parent: self, sprite: self.childNode(withName: "sky") as! SKSpriteNode, speed: 1)
+        forest = EndlessBackground(parent: self, sprite: self.childNode(withName: "forest") as! SKSpriteNode, speed: 3)
+        ground = EndlessBackground(parent: self, sprite: self.childNode(withName: "ground") as! SKSpriteNode, speed: 6)
         
+        girl = self.childNode(withName: "girl") as? SKSpriteNode
     }
     
+    override func update(_ currentTime: TimeInterval) {
+        sky.update()
+        forest.update()
+        ground.update()
+    }
+    
+    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+        girl.physicsBody?.applyImpulse(CGVector(dx: 0, dy: 120))
+    }
     
 }
